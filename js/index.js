255b9e14b44909fdf885cab7957ebdfc7405b36b
Ryan Riley <rr2nz@virginia.edu>
2020-03-03 15:04:55 -0500

    Added Like button, changed tab names

--- js/index.js
+++ js/index.js
@@ -16,3 +16,21 @@ function validateLogin() {
         window.location.href = "dashboard.html";
     }
 }
+
+var likes1 = 0;
+function increaseLike1() {
+  likes1 += 1;
+  document.getElementById("likes1").innerHTML= likes1;
+}
+
+var likes2 = 0;
+function increaseLike2() {
+  likes2 += 1;
+  document.getElementById("likes2").innerHTML= likes2;
+}
+
+var likes3 = 0;
+function increaseLike3() {
+  likes3 += 1;
+  document.getElementById("likes3").innerHTML= likes3;
+}
