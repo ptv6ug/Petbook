6cf4140a63fa75de866c0aef62e48139de50a721
ptv6ug <ptv6ug@virginia.edu>
2020-03-04 17:13:29 -0500

    added very primitive comments section

--- js/index.js
+++ js/index.js
@@ -17,6 +17,26 @@ function validateLogin() {
     }
 }
 
+var commentsCount1 = 0;
+function addComment1() {
+  var commentEntered = document.getElementById("inputComment").value;
+  // Use insertRow(index) method to create an empty <tr> element and add it to the table
+  // table_object.rows.length returns the number of <tr> elements in the collection
+  var tableRef = document.getElementById("commentsTable1");
+  var newRow = tableRef.insertRow(tableRef.rows.length);
+
+  // Prepare content for each cell (or <td>) of the row
+  var newCell = ""; 
+  // Use insertCell(index) method to insert new cells (<td> elements) at the 1st position of the new <tr> element
+  newCell = newRow.insertCell(0);            // specify which column (0)
+  newCell.innerHTML = commentEntered;        // assign content  
+  newCell.onmouseover = this.rowIndex;       // attach row index to the row
+  var commentCounter1 = (x) => x+=1;
+  document.getElementById("commentsCount1").innerHTML= commentCounter1(commentsCount1);
+  commentsCount1 += 1;
+  document.getElementById("inputComment").value = "";
+}
+
 var likes1 = 0;
 function increaseLike1() {
   likes1 += 1;
